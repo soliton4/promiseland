43a6adf5430e82143ebf0cd76e49053d22c73b43
soliton4 <matthias.behrens@gmail.com>
2015-02-28 13:09:52 +0100

    module loader harmonized

--- promiseland.js
+++ promiseland.js
@@ -1,6 +1,6 @@
 // promiseLand
 //
-// Copyright Matthias Behrens 2014
+// Copyright Matthias Behrens 2015
 //
 //
 // promiseLand is a very promising Language
@@ -10,89 +10,7 @@
 
 
 (function(){
-  var defineFun;
-  var requireFun;
-
-  if (typeof exports == "object" && typeof module == "object"){ // CommonJS
-    console.log("commonjs x");
-    requireFun = function(modulesAr, callback){
-      var i = 0;
-      var l = modulesAr.length;
-      var args = [];
-      for (i; i < l; ++i){
-        args.push(require(modulesAr[i]));
-      };
-      callback.apply(callback, args);
-    };
-    defineFun = function(requireAr, callback){
-      requireFun(requireAr, function(){
-        module.exports = callback.apply(callback, arguments);
-      });
-    };
-
-  }else if (typeof define == "function" && define.amd){ // AMD
-    console.log("amd");
-    var _define = define;
-    requireFun = require;
-
-    defineFun = function(par1, par2){
-      if (par1 instanceof Array){
-        par1.unshift("require");
-      }else{
-        par2 = par1;
-        par1 = ["require"];
-      };
-      _define(par1, function(){
-        requireFun = arguments[0];
-        var args = [];
-        for (var i = 1; i < arguments.length; ++i){
-          args.push(arguments[i]);
-        };
-        return par2.apply(null, args);
-      });
-    };
-    requireFun = require;
-
-  }else{ // Plain browser env
-    console.log("plain browser mode");
-    requireFun = function(modulesAr, callback){
-      if (modulesAr && modulesAr.length){
-        throw {
-          msg: "no module loader available"
-        };
-      };
-      callback.apply(callback, []);
-    };
-    defineFun = function(requireAr, callback){
-      requireFun(requireAr, function(){
-        var originalPromiseLand = this["promiseland"];
-        this["promiseland"] = callback.apply(callback, arguments);
-        if (originalPromiseLand){
-          for (var i in originalPromiseLand){
-            if (this["promiseland"][i] === undefined){
-              this["promiseland"][i] = originalPromiseLand[i];
-            };
-          };
-        };
-      });
-    };
-
-  };
-
-  defineFun([
-    "./modules/createClass", 
-    "./modules/Promise",
-    "./modules/Chainable",
-    "./modules/Tracker",
-    "./modules/moduleSystem",
-    "./modules/classSystem",
-    "./modules/frames",
-    "./modules/ParserClass",
-    "./modules/errorMsg"
-  ], function(createClassModule, PromiseModule, ChainableModule, TrackerModule, moduleSystemModule, classSystemModule, framesModule, ParserClassModule, errorMsgModule){
-
-    var require = requireFun;
-
+  var __modFun = function(require, createClassModule, PromiseModule, ChainableModule, TrackerModule, moduleSystemModule, classSystemModule, framesModule, ParserClassModule, errorMsgModule){
 
     // extra modules
 
@@ -247,5 +165,50 @@
 
     return promiseland;
 
-  });
+  };
+  
+  if (typeof exports == "object" && typeof module == "object"){ // CommonJS
+    module.exports = __modFun(function(modulesAr, callback, errBack){
+        // the require function for CommonJs
+        var args = [];
+        try{
+          var i = 0;
+          var l = modulesAr.length;
+          for (i; i < l; ++i){
+            args.push(require(modulesAr[i]));
+          };
+        }catch(e){
+          errBack(e);
+          return;
+        };
+        callback.apply(callback, args);
+      }, require("./modules/createClass"),
+      require("./modules/Promise"),
+      require("./modules/Chainable"),
+      require("./modules/Tracker"),
+      require("./modules/moduleSystem"),
+      require("./modules/classSystem"),
+      require("./modules/frames"),
+      require("./modules/ParserClass"),
+      require("./modules/errorMsg")
+    );
+
+  }else if (typeof define == "function" && define.amd){ // AMD
+    define([
+      "require",
+      "./modules/createClass", 
+      "./modules/Promise",
+      "./modules/Chainable",
+      "./modules/Tracker",
+      "./modules/moduleSystem",
+      "./modules/classSystem",
+      "./modules/frames",
+      "./modules/ParserClass",
+      "./modules/errorMsg"
+    ], __modFun);
+    
+  }else{ // Plain browser env
+    __modFun(function(){ throw { msg: "require not possible in non loader mode" }; });
+  
+  };
 })();
